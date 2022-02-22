6640ae26ad01a6b8319af717450405d9862fd42b
Heri Kaniugu <hekan12@gmail.com>
2022-02-22 11:12:12 +0300

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_WORKSPACE_URL: "https://rpc-mumbai.maticvigil.com/",
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
